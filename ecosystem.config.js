6068646db5172a1acdd7f8d8e0606ca305ddecaa
talk <taleyal831@gmail.com>
2026-04-27 20:34:58 +0300

    chore: generate code coverage report and update project environment configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'web-app-assignment',
       script: 'dist/index.js',
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       env_production: {
         NODE_ENV: 'production',
         PORT: 3000
